4da1a8695d1b7250f35c8cdb1e9613d9e81fc814
Kristijan Miloseski <kristijan.miloseski.f@gmail.com>
2025-06-04 19:02:18 +0200

    start

---
+++ src/main/java/com/mcnz/spring/soaprest/Controllers/ScoreEndpoint.java
@@ -0,0 +1,34 @@
+package com.mcnz.spring.soaprest.Controllers;
+
+import com.mcnz.jee.soap.*;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class ScoreEndpoint {
+
+
+
+    @PayloadRoot(namespace = "http://soap.jee.mcnz.com/", localPart = "getScoreRequest")
+    @ResponsePayload
+    public GetScoreResponse getScore(@RequestPayload GetScoreRequest request) {
+        Score score = new Score();
+        if (request.getUser().equalsIgnoreCase("Scrumtous"))
+        {
+            score.setWins(100);
+        }
+        else
+        {
+            score.setWins(80);
+            score.setLosses(66);
+            score.setTies(7);
+        }
+        GetScoreResponse response = new GetScoreResponse();
+        response.setScore(score);
+        return response;
+    }
+
+
+}
