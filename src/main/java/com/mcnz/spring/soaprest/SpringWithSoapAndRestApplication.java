44ba290dbd5fa8c79349ab2c6d397e2ce84e1ed9
Kristijan Miloseski <kristijan.miloseski.f@gmail.com>
2025-06-04 18:29:38 +0200

    inti

---
+++ src/main/java/com/mcnz/spring/soaprest/SpringWithSoapAndRestApplication.java
@@ -0,0 +1,13 @@
+package com.mcnz.spring.soaprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithSoapAndRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWithSoapAndRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mcnz/spring/soaprest/SpringWithSoapAndRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcnz.spring.soaprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithSoapAndRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
